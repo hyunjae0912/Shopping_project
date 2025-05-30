706a5b86cdae2b3f3221dbf9e60a61fe5f5a6f07
hyunjae <hyunjae00912@nvaer.com>
2025-05-30 12:06:22 +0900

    05_30 추가

--- src/main/java/com/example/demo/user/controller/UserController.java
+++ src/main/java/com/example/demo/user/controller/UserController.java
@@ -2,6 +2,7 @@
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
@@ -17,7 +18,6 @@ public class UserController {
 	
 	@GetMapping("/register")
 	public String register() {
-		
 		return "/user/register";
 	}
 	
@@ -29,10 +29,10 @@ public class UserController {
 			System.out.println("회원가입 성공");
 			return "redirect:/home/login";
 		}
-		else {	
+		else {
 			System.out.println("실패함");
-			redirectAttributes.addFlashAttribute("msg", "아이디가 중복되어 등록에 실패");
-			return "redirect:/user/register";
+			
+			return "redirect:/register";
 		}
 	}
 	
